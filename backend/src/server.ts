0030191a40a36d3d1bdffaa2075635f2efa7945e
João Paulo <musicjoaopaulo@gmail.com>
2020-04-27 14:36:37 -0300

    chore(backend): configure editorconfig, configure eslint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ messager: 'helloW' });
-});
+app.get("/", (req, res) => res.json({ messager: "helloW" }));
 
 app.listen(3333, () => {
-  console.log('📡 Server started on port 3333.');
+  console.log("📡 Server started on port 3333.");
 });
