64ddfc3afe2c53e5a0bf85c379349c87a2b9b7a9
João Paulo <musicjoaopaulo@gmail.com>
2020-04-27 15:36:08 -0300

    chore(backend): configure express to use json, configure debugger

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ messager: 'helloW' }));
+routes.post('/users', (req, res) => {
+  const { name, email } = req.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return res.json(user);
+});
 
 export default routes;
