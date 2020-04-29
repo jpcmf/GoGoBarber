4d3d031af46cdfbc6d126cc0a5e7fd10b99e46f0
João Paulo <musicjoaopaulo@gmail.com>
2020-04-28 22:02:03 -0300

    chore(backend): save and list appointments using the concept of repository typeorm

--- backend/src/database/migrations/1588116851851-CreateAppointments.ts
+++ backend/src/database/migrations/1588116851851-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1588116851851
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
