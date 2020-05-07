c5fd8efad745e7c50fae319c8410471d2c3e9c96
João Paulo <musicjoaopaulo@gmail.com>
2020-05-07 19:05:05 -0300

    chore(backend): create register user and refactor register appointment

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
+import usersRouter from './users.routes';
 import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
+routes.use('/users', usersRouter);
 routes.use('/appointments', appointmentsRouter);
 
 export default routes;
