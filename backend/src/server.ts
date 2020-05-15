a9798319d012219e035d512e9006067dde2eabc7
João Paulo <musicjoaopaulo@gmail.com>
2020-05-15 20:38:53 -0300

    chore(backend): add cors and configure

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -13,6 +14,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
