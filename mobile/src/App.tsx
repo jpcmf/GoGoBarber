9c8312d2cd6ad2ff41035d123ec20e81033e4896
João Paulo <musicjoaopaulo@gmail.com>
2020-05-31 20:51:20 -0300

    feat(mobile): add editorconfig, estlin and prettier config

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
-
-// import { Container } from './styles';
+import { View } from 'react-native';
 
 const App: React.FC = () => {
   return <View />;
