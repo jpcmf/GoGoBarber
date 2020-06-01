a40a50b844f02b75901a39dc91933f25d1784f90
João Paulo <musicjoaopaulo@gmail.com>
2020-05-31 22:05:26 -0300

    chore(mobile): add logo, add custom fonts

---
+++ mobile/react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  project: {
+    ios: {},
+    androind: {},
+  },
+  assets: ['./assets/fonts'],
+};
--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 24px;
+  margin: 64px 0 24px;
+`;
