55bc5b1c131af82846c2b2df4c82ae07946d379b
João Paulo <musicjoaopaulo@gmail.com>
2020-06-08 19:59:24 -0300

    chore(mobile): create signup layout and routes

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logo from '../../assets/logo.png';
+import color from '../../styles/colors';
+
+import { Title, Container, BackButton, BackButtonText } from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logo} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => {}}>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackButton
+        onPress={() => {
+          navigation.goBack();
+        }}
+      >
+        <Icon name="arrow-left" size={20} color={color.white} />
+        <BackButtonText>Já possuo uma conta</BackButtonText>
+      </BackButton>
+    </>
+  );
 };
 
 export default SignUp;
--- mobile/src/pages/SignUp/styles.ts
+++ mobile/src/pages/SignUp/styles.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { Platform } from 'react-native';
 
-export const Container = styled.View``;
+import colors from '../../styles/colors';
+
+export const Container = styled.View`
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 0}px;
+`;
+
+export const Title = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 24px;
+  margin: 64px 0 24px;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  align-items: center;
+  background-color: ${colors.secondary};
+  border-top-color: ${colors.inputBd};
+  border-top-width: 1px;
+  bottom: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  left: 0;
+  padding: 16px 0 ${8 + getBottomSpace()}px;
+  position: absolute;
+  right: 0;
+`;
+
+export const BackButtonText = styled.Text`
+  color: ${colors.white};
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  margin-left: 16px;
+`;
