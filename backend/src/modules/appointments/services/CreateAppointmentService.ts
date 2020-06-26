4a28faa6df693c0dc31eaf57faca87b980742908
João Paulo <musicjoaopaulo@gmail.com>
2020-06-25 22:42:31 -0300

    chore(backend): configure a new architecture

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,10 +1,10 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import Appointment from '../models/Appointment';
-import AppointmentRepository from '../repositories/AppointmentRepository';
+import AppError from '@shared/errors/AppError';
 
-import AppError from '../errors/AppError';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import AppointmentRepository from '../repositories/AppointmentRepository';
 
 interface Request {
   provider_id: string;
