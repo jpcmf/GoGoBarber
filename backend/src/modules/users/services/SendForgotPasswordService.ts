c64df3f9dfb0a0d0a8d22353c7c5bb0fa90d12a0
João Paulo <musicjoaopaulo@gmail.com>
2020-07-07 00:25:45 -0300

    chore(backend): create send forgot password test

---
+++ backend/src/modules/users/services/SendForgotPasswordService.ts
@@ -0,0 +1,32 @@
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+
+import IMailProvider from '@shared/container/providers/MailProdiveder/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+// import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.',
+    );
+  }
+}
+
+export default SendForgotPasswordService;
