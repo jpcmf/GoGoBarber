c699654d1ddb5f41cc19ace79ff88cb0c9d21541
João Paulo <musicjoaopaulo@gmail.com>
2020-07-21 21:18:26 -0300

    chore(backend): create routes and controllers for password

---
+++ backend/src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import SendForgotPasswordService from '@modules/users/services/SendForgotPasswordService';
+
+export default class ForgotPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { email } = req.body;
+
+    const sendForgotPassword = container.resolve(SendForgotPasswordService);
+
+    await sendForgotPassword.execute({
+      email,
+    });
+
+    return res.json(email);
+  }
+}
