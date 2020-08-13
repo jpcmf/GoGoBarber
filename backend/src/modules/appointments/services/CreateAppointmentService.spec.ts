ff1b07b94c9105db354da9bbf659d6648a669d85
João Paulo <musicjoaopaulo@gmail.com>
2020-08-13 11:19:52 -0300

    chore(backend): add user id to the appointments

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,6 +16,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '131313',
       provider_id: '121212',
     });
 
@@ -28,12 +29,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '131313',
       provider_id: '121212',
     });
 
     expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '131313',
         provider_id: '121212',
       }),
     ).rejects.toBeInstanceOf(AppError);
