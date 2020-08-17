c5f2d7758072c4ad4b1e7b0f227b1be8407f72bc
João Paulo <musicjoaopaulo@gmail.com>
2020-08-17 17:22:36 -0300

    chore(backend): create schedule of provider

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 7, 18, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 7, 18, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      day: 18,
+      month: 8,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
